99a9971386fc7bf719892356c22d3ea41bbd5c7a
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2025-05-16 17:25:14 -0300

    feat(api-avatar): traduzindo os campos Name e Affiliation de cada personagem.

--- app/api-avatar.py
+++ app/api-avatar.py
@@ -1,5 +1,18 @@
 import requests, json
+from googletrans import Translator
 
-r = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
+resposta = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
+resposta_json = resposta.json()
+tradutor = Translator()
 
-print((json.dumps(r.json(), indent=4)))
+for personagem in resposta_json:
+    nome = personagem["name"].strip()
+    personagem["name"] = tradutor.translate(nome, src="en", dest="pt").text
+    
+    if ("affiliation" in personagem):
+        afiliacao = personagem["affiliation"].strip().lower()
+        personagem["affiliation"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
+
+
+resposta_formatada = json.dumps(resposta_json, ensure_ascii=False, indent=4)
+print(resposta_formatada)
