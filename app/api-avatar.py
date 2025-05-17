e812daf4bd660d28a45b14a9cac1c27323542f7f
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2025-05-17 09:50:24 -0300

    refactor(api-avatar): melhorando nomes das variáveis.
    Finalizando dia 2.

--- app/api-avatar.py
+++ app/api-avatar.py
@@ -1,11 +1,11 @@
 import requests, json
 from googletrans import Translator
 
-resposta = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
-resposta_json = resposta.json()
+personagens = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
+personagens = personagens.json()
 tradutor = Translator()
 
-for personagem in resposta_json:
+for personagem in personagens:
     nome = personagem["name"].strip()
     personagem["name"] = tradutor.translate(nome, src="en", dest="pt").text
     
@@ -14,5 +14,5 @@ for personagem in resposta_json:
         personagem["affiliation"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
 
 
-resposta_formatada = json.dumps(resposta_json, ensure_ascii=False, indent=4)
+resposta_formatada = json.dumps(personagens, ensure_ascii=False, indent=4)
 print(resposta_formatada)
