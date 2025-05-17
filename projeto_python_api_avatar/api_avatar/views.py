dfc9396a483101d50e7ae3cfdec27727b0544eaf
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2025-05-17 16:09:19 -0300

    feat(projeto_python_api_avatar): criando projeto Django e um app teste da API do Avatar.

---
+++ projeto_python_api_avatar/api_avatar/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests, json
+from googletrans import Translator
+
+
+def index(request):     
+    personagens = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
+    personagens = personagens.json()
+    tradutor = Translator()
+
+    for personagem in personagens:
+        nome = personagem["name"].strip()
+        personagem["name"] = tradutor.translate(nome, src="en", dest="pt").text
+        
+        if ("affiliation" in personagem):
+            afiliacao = personagem["affiliation"].strip().lower()
+            personagem["affiliation"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
+
+    resposta_formatada = json.dumps(personagens, ensure_ascii=False, indent=4)
+    return HttpResponse(resposta_formatada)
