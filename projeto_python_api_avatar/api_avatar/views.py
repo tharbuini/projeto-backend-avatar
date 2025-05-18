aa8c481dc75ab35b2ac9853a938058753a7d4afa
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2025-05-18 11:19:28 -0300

    feat(views.py): traduzindo outros termos em inglês.

--- projeto_python_api_avatar/api_avatar/views.py
+++ projeto_python_api_avatar/api_avatar/views.py
@@ -3,21 +3,33 @@ from django.http import HttpResponse, JsonResponse
 import requests, json
 from googletrans import Translator
 
-
 def index(request):     
     personagens = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
     personagens = personagens.json()
     tradutor = Translator()
 
     for personagem in personagens:
-        nome = personagem["name"].strip()
-        personagem["name"] = tradutor.translate(nome, src="en", dest="pt").text
+        personagem["aliados"] = personagem.pop("allies")
+        personagem["inimigos"] = personagem.pop("enemies")
+        personagem["nome"] = personagem.pop("name")
+        
+        nome = personagem["nome"].strip()
+        personagem["nome"] = tradutor.translate(nome, src="en", dest="pt").text
+        aliados = personagem["aliados"]
+        personagem["aliados"] = [
+            tradutor.translate(aliado, src="en", dest="pt").text for aliado in aliados
+        ]
+        inimigos = personagem["inimigos"]
+        personagem["inimigos"] = [
+            tradutor.translate(inimigo, src="en", dest="pt").text for inimigo in inimigos
+        ]
         
         if ("affiliation" in personagem):
-            afiliacao = personagem["affiliation"].strip().lower()
-            personagem["affiliation"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
+            personagem["afiliacao"] = personagem.pop("affiliation")
+            afiliacao = personagem["afiliacao"].strip().lower()
+            personagem["afiliacao"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
 
     resposta_formatada = json.dumps(personagens, ensure_ascii=False, indent=4)
-    return HttpResponse(f"<pre>{resposta_formatada}</pre>", content_type="text/html")
+    return HttpResponse(f"<pre>{resposta_formatada}</pre>", content_type="text/html; charset=utf-8")
     
     # JSON PURO -> return JsonResponse(personagens, safe=False, json_dumps_params={"ensure_ascii": False, "indent": 4})
