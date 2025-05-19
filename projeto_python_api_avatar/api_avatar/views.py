c5b4211384ad66bc6fe47e40fae672c51d5ecd9a
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2025-05-19 15:03:08 -0300

    feat(api_avatar): criando template e melhorando código do views

--- projeto_python_api_avatar/api_avatar/views.py
+++ projeto_python_api_avatar/api_avatar/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse, JsonResponse
 import requests, json
 from googletrans import Translator
 
-def index(request):     
-    personagens = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
-    personagens = personagens.json()
+def personagens(request):  
+    api_url = 'https://last-airbender-api.fly.dev/api/v1/characters'
+    lista_personagens = requests.get(api_url)
+    lista_personagens = lista_personagens.json()
     tradutor = Translator()
 
-    for personagem in personagens:
+    for personagem in lista_personagens:
         personagem["aliados"] = personagem.pop("allies")
         personagem["inimigos"] = personagem.pop("enemies")
         personagem["nome"] = personagem.pop("name")
@@ -29,7 +30,6 @@ def index(request):
             afiliacao = personagem["afiliacao"].strip().lower()
             personagem["afiliacao"] = tradutor.translate(afiliacao, src="en", dest="pt").text.capitalize()
 
-    resposta_formatada = json.dumps(personagens, ensure_ascii=False, indent=4)
-    return HttpResponse(f"<pre>{resposta_formatada}</pre>", content_type="text/html; charset=utf-8")
+    return render(request, "index.html", {'personagens': lista_personagens})
     
     # JSON PURO -> return JsonResponse(personagens, safe=False, json_dumps_params={"ensure_ascii": False, "indent": 4})
